0f06a108b2f1dae3afb354b2d15c365646cd7d91
ryzasurya <ezasurya234@gmail.com>
2024-12-13 14:36:14 +0700

    Add files via upload

---
+++ variabel_float.cpp
@@ -0,0 +1,15 @@
+#include <iostream.h>
+#include <conio.h>
+int main ()
+{
+// declaring variables:
+float a, b;
+float result;
+// process:
+a = 21;
+b = 2;
+result = a / b;
+cout << "Hasil (float)= " << result;
+getch();
+return 0;
+}
