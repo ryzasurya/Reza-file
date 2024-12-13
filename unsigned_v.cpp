45eeac43cc6065c4753eca4068be72308b19508a
ryzasurya <ezasurya234@gmail.com>
2024-12-13 15:01:44 +0700

    Add files via upload

---
+++ unsigned_v.cpp
@@ -0,0 +1,13 @@
+#include <iostream.h>
+#include <conio.h>
+int main ()
+{
+unsigned int jumlah, harga;
+int total;
+jumlah = 13; // jumlah >= 0
+harga = 56700; // harga >= 0
+total = jumlah * harga;
+cout << "Total = " << total;
+getch();
+return 0;
+}
